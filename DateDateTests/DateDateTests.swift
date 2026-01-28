b2ab0a34c1325c656cdcc5bc6f870ce145f89657
Harold <20009626+HaroldGin931@users.noreply.github.com>
2026-01-28 17:24:46 +0800

    Initial Commit

---
+++ DateDateTests/DateDateTests.swift
@@ -0,0 +1,17 @@
+//
+//  DateDateTests.swift
+//  DateDateTests
+//
+//  Created by Harold on 2026/1/28.
+//
+
+import Testing
+@testable import DateDate
+
+struct DateDateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
